5f466ef170af88b1cc9b132a97c1c863b1869119
John LaMaster <7785925+JohnLaMaster@users.noreply.github.com>
2020-10-22 15:19:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from cluster_detection import *
-from dropCluster import *
-from scheduler import *
+from .cluster_detection import *
+from .dropCluster import *
+from .scheduler import *
 
 __all__ = ['dropCluster', 'cluster_detection', 'scheduler']
